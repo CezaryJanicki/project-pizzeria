0fc94ab45efee672c5820d5344a850efd0ee85b4
CezaryJanicki <cezary.janicki@gmail.com>
2023-04-13 16:54:05 +0200

    Remove product from cart

--- dist/js/script.js
+++ dist/js/script.js
@@ -365,7 +365,7 @@
     }
     announce() {
       const thisWidget = this;
-      const event = new Event('updated', {
+      const event = new CustomEvent('updated', {
         bubbles: true
       });
       thisWidget.element.dispatchEvent(event);
@@ -411,6 +411,9 @@
       thisCart.dom.productList.addEventListener('updated', function(){
         thisCart.update();
       });
+      thisCart.dom.productList.addEventListener('remove', function(){
+        thisCart.remove(event.detail.cartProduct);
+      });
     }
     add(menuProduct) {
       const thisCart = this;
@@ -449,6 +452,14 @@
           elem.innerHTML = thisCart[key];
         }
       }
+    }
+    remove(cartProduct) {
+      const thisCart = this;
+
+      const index = thisCart.products.indexOf(cartProduct);
+      thisCart.products.splice(index, 1);
+      cartProduct.dom.wrapper.remove();
+      thisCart.update();
 
     }
   }
@@ -467,6 +478,7 @@
       thisCartProduct.getElements(element);
       thisCartProduct.initAmountWidget();
       console.log('new CartProduct; ', thisCartProduct);
+      thisCartProduct.initActions();
     }
     getElements(element){
       const thisCartProduct = this;
@@ -488,6 +500,28 @@
         console.log('thisCartProduct price is: ', thisCartProduct.price);
         thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
       });
+    }
+    remove() {
+      const thisCartProduct = this;
+      const event = new CustomEvent('remove', {
+        bubbles: true,
+        detail: {
+          cartProduct: thisCartProduct,
+        },
+      });
+      thisCartProduct.dom.wrapper.dispatchEvent(event);
+    }
+    initActions() {
+      const thisCartProduct = this;
+      thisCartProduct.dom.edit.addEventListener('click', function(event) {
+        event.preventDefault();
+      });
+      thisCartProduct.dom.remove.addEventListener('click', function(event) {
+        event.preventDefault();
+        thisCartProduct.remove();
+        console.log('cart products was removed!)');
+      });
+
     }
   }
   const app = {
